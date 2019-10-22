cea5899a9af0a455a01efa3f94c5196aad4026f9
andreiolarasu <olarasu.andrei@gmail.com>
2019-10-22 15:48:55 +0200

    Add project files.

---
+++ CookieAPI/Controllers/CookiesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using CookieAPI.Models;
+
+namespace CookieAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CookiesController : ControllerBase
+    {
+        [HttpPost]
+        public void AddPreferences(CookiePreferencesModel data)
+        {
+            var preferences = data;
+
+            preferences.Id = Guid.NewGuid();
+            preferences.Created = DateTime.Now;
+
+            DAL.DAL.AddCookiePreferences(preferences);
+
+        }
+    }
+}
